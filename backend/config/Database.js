436611b343123f382205fc6ff8fcd0b474f9e1bf
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-10-04 19:33:45 +0700

    enhance flow

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,14 @@
-import { Sequelize } from "sequelize";
+import Sequelize from 'sequelize';
 
-const db = new Sequelize('sipbogev02', 'root','', {
-    host:"localhost",
-    dialect: "mysql"
-});
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE || 'sipbogev2',
+  process.env.MYSQL_USER || 'sipbogev2',
+  process.env.MYSQL_PASSWORD || 'root',
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  },
+);
 
-export default db;
+export default sequelize;
